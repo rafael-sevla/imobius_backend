4b6a45a3d03577d5aeca59c13d1389e8afb21d0e
Rafael Alves <sevla.rafael@gmail.com>
2023-12-27 15:51:23 -0300

    Class 1

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates_uniqueness_of :username
+  validates_presence_of :name, :username, :email
+
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+end
---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+# Create a default user
+User.find_or_create_by!(name: 'Admin', username: 'admin', email: 'admin@admin.com') do |user|
+  user.password = '123456'
+  user.save!
+end
+
+Doorkeeper::Application.find_or_create_by(name: "Public") do | app |
+  app.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  app.confidential = true
+  app.scopes = "public"
+  app.save!
+end
+
+Doorkeeper::Application.find_or_create_by(name: "Admin") do | app |
+  app.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  app.confidential = false
+  app.scopes = "admin"
+  app.save!
+end
